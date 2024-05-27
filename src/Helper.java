7d1f0a2f7c1eb2e2a4b772a3ba24ebc5f73c6a18
MEisSCAMMER <ishaan.ramesh@gmail.com>
2024-05-27 12:44:58 -0400

    taking stuff: full support! also, more parsing
    also, javadocs

--- src/Helper.java
+++ src/Helper.java
@@ -12,6 +12,32 @@ public class Helper {
             String.format("src%sdata%snoun_data_original.tsv", File.separator, File.separator);
     public static final int ACCENT_COLOR = 34;
 
+    /**
+     * @param x the x-coordinate of the noun(s).
+     * @param y the y-coordinate of the noun(s).
+     * @return an ArrayList of the noun(s).
+     */
+    private static ArrayList<Noun> getNouns(int x, int y) {
+        ArrayList<Noun> nouns = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File(NOUN_DATA_FILE))) {
+            scanner.nextLine(); //skip the header
+            while(scanner.hasNextLine()) {
+                String[] data = scanner.nextLine().split("\t");
+                if(Integer.parseInt(data[0])==x && Integer.parseInt(data[1])==y)
+                    nouns.add(new Noun(data[2], data[3].split(","), data[4], Boolean.parseBoolean(data[5])));
+            }
+        } catch (IOException e) {
+            System.out.println("Oops, there's been an error: " + e.getMessage());
+        }
+
+        return nouns;
+    }
+
+    /**
+     * @param x the x-coordinate of the location.
+     * @param y the y-coordinate of the location.
+     * @return a Location object representing the location at the specified coordinates.
+     */
     public static Location getLoc(int x, int y) {
         try (Scanner scanner = new Scanner(new File(ROOM_DATA_FILE))) {
             scanner.nextLine(); //skip the header
@@ -33,26 +59,20 @@ public class Helper {
         return null;
     }
 
-    private static ArrayList<Noun> getNouns(int x, int y) {
-        ArrayList<Noun> nouns = new ArrayList<>();
-        try(Scanner scanner = new Scanner(new File(NOUN_DATA_FILE))) {
-            scanner.nextLine(); //skip the header
-            while(scanner.hasNextLine()) {
-                String[] data = scanner.nextLine().split("\t");
-                if(Integer.parseInt(data[0])==x && Integer.parseInt(data[1])==y)
-                    nouns.add(new Noun(data[2], data[3].split(","), data[4], Boolean.parseBoolean(data[5])));
-            }
-        } catch (IOException e) {
-            System.out.println("Oops, there's been an error: " + e.getMessage());
-        }
-
-        return nouns;
-    }
-
+    /**
+     * @param loc an array containing the x and y coordinates of the location.
+     * @return a Location object representing the location at the specified coordinates.
+     */
     public static Location getLoc(int[] loc) {
         return getLoc(loc[0], loc[1]);
     }
 
+    /**
+     * Checks if there is a location at the specified coordinates.
+     * @param x the x-coordinate of the location.
+     * @param y the y-coordinate of the location.
+     * @return true if the location exists, false otherwise.
+     */
     public static boolean locExists(int x, int y) {
         try (Scanner scanner = new Scanner(new File(ROOM_DATA_FILE))) {
             scanner.nextLine(); //skip the header
@@ -66,21 +86,55 @@ public class Helper {
         return false;
     }
 
-    public static void addNoun(int x, int y, Noun noun) { //todo: clean up so it's not such a hot mess to understand
-        try {
-            Scanner scanner = new Scanner(new File(NOUN_DATA_FILE));
+    /**
+     * Adds a noun to NOUN_DATA_FILE at the specified coordinates.
+     * @param x the x-coordinate to add the noun to.
+     * @param y the y-coordinate to add the noun to.
+     * @param noun the noun to add.
+     */
+    public static void addNoun(int x, int y, Noun noun) {
+        //essentially, what we're trying to do here is get a StringBuilder and build it up so it's exactly the same as
+        //the contents of NOUN_DATA_FILE, BUT with a new noun at the end. then write that back to NOUN_DATA_FILE
+        try (Scanner scanner = new Scanner(new File(NOUN_DATA_FILE))) { //try with resources!
+                                                                        //don't have to worry about closing the scanner
             StringBuilder data = new StringBuilder();
-            do data.append(scanner.nextLine()).append("\n");
-            while (scanner.hasNextLine());
-            data.append(x).append("\t").append(y).append("\t").append(noun.getName()).append("\t");
-            for(String shortName: noun.getShortNames()) data.append(shortName).append(",");
-            data.append("\t").append(noun.getDesc()).append("\t").append(noun.isTakeable()).append("\n");
-            scanner.close();
-            try {
-                java.nio.file.Files.writeString(java.nio.file.Paths.get(NOUN_DATA_FILE), data.toString());
-            } catch (IOException e) {
-                System.out.println("Oops, there's been an error: " + e.getMessage());
+            while (scanner.hasNextLine()) data.append(scanner.nextLine()).append("\n");
+            //now we've finished writing the entire contents of noun_data.tsv to data, it's time to add that noun
+
+            data.append(x).append("\t") //x
+                .append(y).append("\t") //y
+                .append(noun.getName()).append("\t"); //name
+            for(String shortName: noun.getShortNames()) data.append(shortName).append(","); //shortnames
+            data.deleteCharAt(data.length()-1); //delete that last comma
+            data.append("\t")
+                .append(noun.getDesc()).append("\t") //description
+                .append(noun.isTakeable()); //is takeable
+
+            java.nio.file.Files.writeString(java.nio.file.Paths.get(NOUN_DATA_FILE), data.toString());
+        } catch (IOException e) {
+            System.out.println("Oops, there's been an error: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Removes a noun at the specified coordinates from NOUN_DATA_FILE.
+     * @param x the x-coordinate of the noun.
+     * @param y the y-coordinate of the noun.
+     * @param noun the noun to remove.
+     */
+    public static void removeNoun(int x, int y, Noun noun) {
+        //more straightforward than addNoun
+        try (Scanner scanner = new Scanner(new File(NOUN_DATA_FILE))) {
+            StringBuilder data = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                //here, we only append the line IF it's not the noun we want to remove
+                if(!line.startsWith(x + "\t" + y + "\t" + noun.getName())) data.append(line).append("\n");
             }
+
+            data.deleteCharAt(data.length()-1); //and delete that last newline
+
+            java.nio.file.Files.writeString(java.nio.file.Paths.get(NOUN_DATA_FILE), data.toString());
         } catch (IOException e) {
             System.out.println("Oops, there's been an error: " + e.getMessage());
         }
--- src/Parser.java
+++ src/Parser.java
@@ -5,13 +5,20 @@ public class Parser {
     private final Player player;
     private static final String[] VERBS = {"take", "get", "use", "drop", "examine", "x"};
 
-    private static final String[] UNIMPORTANT_WORDS = {"the", "a", "an", "I", "my"};
+    private static final String[] UNIMPORTANT_WORDS = {"the", "a", "an", "i", "my"};
 
     public Parser(Player player) { this.player = player; }
 
+    /**
+     * Parses the input string and performs the appropriate action.
+     * @param input the input string to parse.
+     * @return false if the player has quit, true otherwise.
+     */
     public boolean parse(String input) {
         ArrayList<String> words = new ArrayList<>(List.of(input.split(" ")));
         words.replaceAll(String::toLowerCase);
+        //remove punctuation
+        words.replaceAll(word -> word.replaceAll("[^a-zA-Z0-9]", ""));
         words.removeIf(word -> List.of(UNIMPORTANT_WORDS).contains(word));
         if(words.size() == 2) parseTwoWords(words.get(0), words.get(1));
         else if(words.size() == 1) return parseSingleWord(words.get(0));
@@ -19,6 +26,9 @@ public class Parser {
         return true;
     }
 
+    /**
+     * Prints the current contents of the player's inventory.
+     */
     public void printPlayerInventory() {
         if(player.getInventory().isEmpty()) {
             System.out.println("You are empty-handed. Even your sense of adventure has left you.");
@@ -51,20 +61,26 @@ public class Parser {
 
     public static void printHelp(int color) {
         System.out.printf("""
-                Miniscule Cave Adventure uses a text parser. Here are some commands you can use:
+                You are playing Miniscule Cave Adventure (MCA) v0.0.1.
+                MCA uses a text parser. Here are some commands you can use:
                 * \u001b[%dmN/S/E/W\u001b[0m to move
                 * \u001b[%dmL\u001b[0m or \u001b[%dmLOOK\u001b[0m to print a description of your current location
                 * \u001b[%dmI\u001b[0m or \u001b[%dmINVENTORY\u001b[0m to print the current contents of your inventory
                 * \u001b[%dmTAKE/GET [object]\u001b[0m to take an object
-                * \u001b[%dmDROP [object]\u001b[0m to drop an object. Still ironing out the kinks in this and \u001b[%dmTAKE\u001b[0m
+                * \u001b[%dmDROP [object]\u001b[0m to drop an object
                 * \u001b[%dmUSE [object]\u001b[0m to use an object. It doesn't do much yet but it works, technically
-                * \u001b[%dmX/EXAMINE [object]\u001b[0m to examine an object. Also TBI
+                * \u001b[%dmX/EXAMINE [object]\u001b[0m to examine an object
                 * \u001b[%dmQ\u001b[0m or \u001b[%dmQUIT\u001b[0m to... GEE I DON'T KNOW... quit the game
                 * \u001b[%dmH\u001b[0m or \u001b[%dmHELP\u001b[0m to print this message again%n""",
-                color, color, color, color, color, color, color, color, color, color, color, color, color, color
+                color, color, color, color, color, color, color, color, color, color, color, color, color
         );
     }
 
+    /**
+     * Parses a single-word command.
+     * @param word the word to parse.
+     * @return false if the player has quit, true otherwise.
+     */
     private boolean parseSingleWord(String word) {
         switch (word) {
             case "i", "inventory" -> printPlayerInventory();
@@ -82,44 +98,53 @@ public class Parser {
         return true;
     }
 
+    /**
+     * Parses a two-word command.
+     * @param word1 the first word.
+     * @param word2 the second word.
+     */
     private void parseTwoWords(String word1, String word2) {
+        //we're assuming the player entered something in the form of VERB NOUN
         if(List.of(VERBS).contains(word1)) {
-            //do something with the verb
+            //assemble a list of everything the player could be talking about
             ArrayList<Noun> allNouns = new ArrayList<>();
             allNouns.addAll(player.getLocation().getNouns());
             allNouns.addAll(player.getInventory());
             for(Noun noun: allNouns) for(String name: noun.getShortNames()) {
-                if(name.equals(word2)) {
-                    //verb the noun
-                    switch (word1) {
-                        case "use" -> {
-                            noun.use();
-                            return;
-                        } case "get", "take" -> {
-                            if (player.getLocation().getNouns().contains(noun)) {
+                if(name.equals(word2)) switch (word1) {
+                    case "use" -> { //probably make this do something more useful at some point?
+                        noun.use();
+                        return;
+                    } case "get", "take" -> {
+                        if (player.getLocation().getNouns().contains(noun)) {
+                            if(noun.isTakeable()) {
+                                //we only get this far if the noun is in the room and it's takeable
                                 player.addToInventory(noun);
                                 player.getLocation().removeNoun(noun);
-                                //todo: remove noun from noun_data.tsv
+                                Helper.removeNoun(player.getLocation().getX(), player.getLocation().getY(), noun);
                                 System.out.println("You take the " + noun.getShortName() + ".");
-                            } else System.out.println("You already have the " + noun.getShortName() + ".");
-                            return;
-                        } case "drop" -> {
-                            if (player.getInventory().contains(noun)) {
-                                player.removeFromInventory(noun);
-                                player.getLocation().getNouns().add(noun);
-                                //this works now. hooray!
-                                Helper.addNoun(player.getLocation().getX(), player.getLocation().getY(), noun);
-                                System.out.println("You drop the " + noun.getShortName() + ".");
-                            } else System.out.println("You don't have the " + noun.getShortName() + ".");
-                            return;
-                        } case "examine", "x" -> {
-                            System.out.println(noun.getDesc());
-                            return;
-                        } default -> {}
-                    }
+                            } else System.out.println("You can't take that. It's " + noun.getName() + ", for" +
+                                    " God's sake."); //it's in the room but not takeable
+                        } else System.out.println("You already have the " + noun.getShortName() + ".");
+                               //i.e., it's in the player's inventory but NOT the room
+                        return;
+                    } case "drop" -> {
+                        if (player.getInventory().contains(noun)) {
+                            player.removeFromInventory(noun);
+                            player.getLocation().getNouns().add(noun);
+                            Helper.addNoun(player.getLocation().getX(), player.getLocation().getY(), noun);
+                            System.out.println("You drop the " + noun.getShortName() + ".");
+                        } else System.out.println("You don't have the " + noun.getShortName() + ".");
+                        return;
+                    } case "examine", "x" -> { //this one's easy
+                        System.out.println(noun.getDesc());
+                        return;
+                    } default -> {}
                 }
             }
-            System.out.println("You bozo! There is no " + word2 + "!"); //we haven't found the noun
+            //at this point, the noun isn't in either the player's inventory or the current room
+            //we may now insult the player
+            System.out.println("You bozo! There is no " + word2 + "!");
         } else cantUnderstand();
     }
 }
